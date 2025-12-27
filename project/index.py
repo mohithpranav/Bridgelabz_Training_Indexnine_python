df2c9ac6d6b4210308aaa707664124157155418e
Mohith Pranav <pranavmohith@gmail.com>
2025-12-27 14:34:47 +0530

    review 1 ai_ml

--- project/index.py
+++ project/index.py
@@ -1,1 +1,19 @@
-print("Hello, World!")
+class SeatBookingSystem():
+    def __init__(self, total_seats ):
+        self.total_seats = total_seats
+        self.booked_seats = set()
+
+    def book_seat(self, seat_number):
+        if seat_number < 1 or seat_number > self.total_seats:
+            return "invalid seat number."
+        if seat_number in self.booked_seats:
+            return "Seat already booked."
+        self.booked_seats.add(seat_number)
+        return "seat booked successfully."
+    
+
+    
+s1 = SeatBookingSystem(10)
+print(s1.book_seat(3))  
+
+print(s1.book_seat(3))
---
+++ project/readme.md
@@ -0,0 +1,7 @@
+Create a class SeatBookingSystem:
+
+1. Method book_seat(seat_number)
+   Total seats = 10
+2. Raise exception if:
+   2.1 Seat number invalid
+   2.2 Seat already booked
